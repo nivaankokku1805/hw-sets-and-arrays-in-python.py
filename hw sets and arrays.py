f583c82d092d62bd5f27716939bf806c1c8b0d48
nivaankokku1805 <reetu18@gmail.com>
2025-09-21 10:04:02 +0530

    Add files via upload

---
+++ hw sets and arrays.py
@@ -0,0 +1,21 @@
+# Define two sets
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5, 6}
+
+# Find symmetric difference
+result = set1.symmetric_difference(set2)
+
+# Print the result
+print("Symmetric Difference:", result)
+
+###########################################################################################
+
+# Define two sets
+set1 = {10, 20, 30, 40}
+set2 = {30, 40, 50, 60}
+
+# Find symmetric difference using ^ operator
+result = set1 ^ set2
+
+# Print the result
+print("Symmetric Difference:", result)
